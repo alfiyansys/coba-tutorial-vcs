ae11a3323abd90cca9970c3b3488514b5b30a07e
M Alfiyan S <alfiyan.syamsuddin@gmail.com>
2016-08-15 23:43:23 +0700

    tidak tampil f1

--- coba.c
+++ coba.c
@@ -4,7 +4,6 @@ int jumlah(int,int);
 
 int main(){
 	printf("Hello world!\n");
-	f1();
 	printf("%d + %d = %d\n",4,6,jumlah(4,6));
 	printf("Program ini dikembangkan oleh developer A\n");
 	printf("Program ini dibuat oleh developer B\n");
