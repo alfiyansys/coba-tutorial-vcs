11a4e80654d7d0f5befeb88f43cb8f4369ae37a1
M Alfiyan S <alfiyan.syamsuddin@gmail.com>
2016-08-15 23:14:43 +0700

    tambah fitur penjumlahan

--- coba.c
+++ coba.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 void f1();
+int jumlah(int,int);
 
 int main(){
 	printf("Hello world!\n");
 	f1();
+	printf("%d + %d = %d\n",4,6,jumlah(4,6));
 	return 0;
 }
 
 void f1(){
 	printf("Ini adalah fungsi F1\n");
+}
+
+int jumlah(int a, int b){
+	return a+b;
 }
